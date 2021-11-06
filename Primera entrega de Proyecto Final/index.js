614c883faf367a7338daa46724bbd3648ee9093d
Nicolas Ala <nicoalmeda@gmail.com>
2021-11-06 12:04:17 -0300

    primera entrega hecho con objetos y arrays

--- Primera entrega de Proyecto Final/index.js
+++ Primera entrega de Proyecto Final/index.js
@@ -1,61 +1,109 @@
-const impuestos = parseInt(prompt("Ingrese el gasto de impuestos"));
-
-const gastoEmpleados = parseInt(prompt("Ingrese el gasto de los empleados "));
-
-let gastoTotal = impuestos + gastoEmpleados;
-
-document.getElementById("gastos").innerHTML = gastoTotal;
-
-var cant = parseInt(prompt("Ingrese la cantidad de días"));
-
-function gananciaTotal(cant, gasto3) {
-  var sumador = 0;
-  var gasto3 = gastoTotal;
-  let dias = cant;
-
-  for (dias; dias > 0; dias--) {
-    var ganancias = parseInt(prompt("Ingrese las ganancias del dia " + cant));
-    console.log(ganancias);
-    if (ganancias > gasto3) {
-      let total = ganancia(ganancias, gasto3, dias);
-      sumador = sumador + total;
-    } else {
-      let perdida = perdidas(ganancias, gasto3, dias);
-      sumador = sumador + perdida;
-    }
+class Presupuesto {
+  constructor(impuestos, gastoEmpleados, dias) {
+    this.impuestos = impuestos;
+    this.gastoEmpleados = gastoEmpleados;
+    this.dias = dias;
+    this.gastoTotal = this.impuestos + this.gastoEmpleados;
+    this.cantDias = [];
+    this.presupuesto = 0;
   }
 
-  return sumador;
+  gananciaTotal() {
+    let sumador = 0;
+    let cant = this.dias;
+
+    for (cant; cant > 0; cant--) {
+      let ganancias = parseInt(prompt("Ingrese las ganancias del dia " + cant));
+      console.log(ganancias);
+      if (ganancias > this.gastoTotal) {
+        const div = document.createElement("div");
+        div.className = "elemento";
+        console.log("Este día fue positivo");
+
+        let total = ganancias - this.gastoTotal;
+        console.log(
+          "Las ganancias del día" + " " + cant + " " + "fue" + " " + total
+        );
+
+        div.innerHTML = `Las ganancias del dia ${cant} fue de ${total} `;
+        document.body.appendChild(div);
+
+        this.cantDias.push(total);
+        sumador = sumador + total;
+      } else {
+        const div = document.createElement("div");
+        div.className = "elemento";
+        console.log("El día fue negativo ");
+
+        let perdida = ganancias - this.gastoTotal;
+        console.log(
+          "La perdida total del dia" + " " + cant + " " + "fue" + " " + perdida
+        );
+        div.innerHTML = `La perdida del día ${cant} fue de ${perdida} `;
+        document.body.appendChild(div);
+        this.cantDias.push(perdida);
+
+        sumador = sumador + perdida;
+      }
+    }
+
+    this.presupuesto = sumador;
+  }
+
+  ganancia(ganancias, cant) {
+    const div = document.createElement("div");
+    div.className = "elemento";
+    console.log("Este día fue positivo");
+
+    let total = ganancias - this.gastoTotal;
+    console.log(
+      "Las ganancias del día" + " " + cant + " " + "fue" + " " + total
+    );
+
+    div.innerHTML = `Las ganancias del dia ${dias} fue de ${total} `;
+    document.body.appendChild(div);
+
+    this.cantDias.push(total);
+    return total;
+  }
+
+  perdidas(ganancias, cant) {
+    const div = document.createElement("div");
+    div.className = "elemento";
+    console.log("El día fue negativo ");
+
+    let perdida = ganancias - this.gastoTotal;
+    console.log(
+      "La perdida total del dia" + " " + cant + " " + "fue" + " " + perdida
+    );
+    div.innerHTML = `La perdida del día ${dias} fue de ${perdida} `;
+    document.body.appendChild(div);
+    this.cantDias.push(perdida);
+    return perdida;
+  }
+
+  mostrarGastoTotal() {
+    document.getElementById("gastos").innerHTML = this.gastoTotal;
+  }
+
+  mostrarGananciaTotal() {
+    alert("Las ganacias totales de los días ingresados es " + this.presupuesto);
+
+    document.getElementById("ganancia").innerHTML = this.presupuesto;
+  }
+
+  mostrarCantDias() {
+    this.cantDias.forEach((element) => console.log(element + " "));
+  }
 }
 
-function ganancia(ganancias, gasto3, dias) {
-  const div = document.createElement("div");
-  div.className = "elemento";
-  console.log("Este día fue positivo");
+const presupuesto1 = new Presupuesto(
+  parseFloat(prompt("Ingrese el gasto de impuestos")),
+  parseInt(prompt("Ingrese el gasto de los empleados ")),
+  parseInt(prompt("Ingrese la cantidad de dias "))
+);
 
-  let total = ganancias - gasto3;
-  console.log("Las ganancias del día" + " " + cant + " " + "fue" + " " + total);
-
-  div.innerHTML = `Las ganancias del dia ${dias} fue de ${total} `;
-  document.body.appendChild(div);
-  return total;
-}
-
-function perdidas(ganancias, gasto3, dias) {
-  const div = document.createElement("div");
-  div.className = "elemento";
-  console.log("El día fue negativo ");
-
-  let perdida = ganancias - gasto3;
-  console.log(
-    "La perdida total del dia" + " " + cant + " " + "fue" + " " + perdida
-  );
-  div.innerHTML = `La perdida del día ${dias} fue de ${perdida} `;
-  document.body.appendChild(div);
-  return perdida;
-}
-
-let mostrar = gananciaTotal(cant);
-alert("Las ganacias totales de los días ingresados es " + mostrar);
-
-document.getElementById("ganancia").innerHTML = mostrar;
+presupuesto1.gananciaTotal();
+presupuesto1.mostrarGananciaTotal();
+presupuesto1.mostrarGastoTotal();
+presupuesto1.mostrarCantDias();
